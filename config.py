2f47bdd583ece697bc9c9202b6e16faee906a86a
rootgamer-glitch <88827345+rootgamer-glitch@users.noreply.github.com>
2021-09-11 09:02:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,18 +42,18 @@ else:
     finalurl=STREAM
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", '')
+    ADMIN = os.environ.get("ADMINS", '969602163')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    API_ID = int(os.environ.get("API_ID", '6235351'))
+    CHAT = int(os.environ.get("CHAT", "-1001251594119"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001500581255")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
-    ARQ_API=os.environ.get("ARQ_API", "")
+    ARQ_API=os.environ.get("ARQ_API", "GKSRZD-DYNBIF-CGCAKE-ZYWUDK-ARQ")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
     if REPLY_MESSAGE:
         REPLY_MESSAGE=REPLY_MESSAGE
@@ -64,9 +64,8 @@ class Config:
         EDIT_TITLE=None
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_HASH = os.environ.get("API_HASH", "f68fde1378da8f85a243f2ae57f2fcf9")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1970049643:AAGWHMdfK0831gl8MB8_-L3Krws2hwIxaK4") 
+    SESSION = os.environ.get("SESSION_STRING", "1BQANOTEuMTA4LjU2LjEzMAG7eDcUB54pE6j7RxkJ7Ogc2J39GAyGtY9MfQRB0hwtcHi7N/jK4v1Kg5CfxeZBke5L9V/MrFknbfJhpbqG5Ph8x67vwvkI2MAG/euPRaRt1HYHoVROGTnRV16VP0CASS9g7g7WIz5sWWUoL2kpxEklzuEB3pJl1qeBAvxUqHR6UhyD2MauPsHDIdQEXS7PPvOgCxfqcX+HAKIerCPwZy6b9Er6NYiMmGhUCzpOc81IiYe1oZpboHW87B9QRocP+X8foi8IRas6Z4GwXaTQdlJICu2jZPxlD8MuehaqEXfBeM0cAv5XLgx/yJPxaB7vb11irQqm+CEoJl6Mo1Zl/p9MOg==")
     playlist=[]
     msg = {}
-
